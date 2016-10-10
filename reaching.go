9da33286dd0b10439cffd7469d0fb4ac9ab06034
Nick Johnson <arachnid@notdot.net>
2016-10-10 19:18:56 +0100

    Fix stack implementation so it can be used as a map key

--- reaching.go
+++ reaching.go
@@ -2,6 +2,8 @@ package evmdis
 
 import (
 	"fmt"
+	"log"
+	"github.com/arachnid/evmdis/stack"
 )
 
 type InstructionPointer struct {
@@ -9,11 +11,11 @@ type InstructionPointer struct {
 	OriginIndex int
 }
 
-func (self *InstructionPointer) Get() *Instruction {
+func (self InstructionPointer) Get() *Instruction {
 	return &self.OriginBlock.Instructions[self.OriginIndex]
 }
 
-func (self *InstructionPointer) GetAddress() int {
+func (self InstructionPointer) GetAddress() int {
 	address := self.OriginBlock.Offset
 	for i := 0; i < self.OriginIndex; i++ {
 		address += self.OriginBlock.Instructions[i].Op.OperandSize() + 1
@@ -21,7 +23,7 @@ func (self *InstructionPointer) GetAddress() int {
 	return address
 }
 
-func (self *InstructionPointer) String() string {
+func (self InstructionPointer) String() string {
 	return fmt.Sprintf("%v@0x%X", self.Get(), self.GetAddress())
 }
 
@@ -47,14 +49,14 @@ type ReachingDefinition []InstructionPointerSet
 type reachingState struct {
 	program 	*Program
 	nextBlock	*BasicBlock
-	stack		*StackFrame
+	stack		stack.StackFrame
 }
 
 func PerformReachingAnalysis(prog *Program) error {
-	initial := &reachingState{
+	initial := reachingState{
 		program: prog,
 		nextBlock: prog.Blocks[0],
-		stack: nil,
+		stack: stack.StackEnd{},
 	}
 	return ExecuteAbstractly(initial)
 }
@@ -75,16 +77,17 @@ func updateReachings(inst *Instruction, operands []InstructionPointer) {
 	inst.Annotations.Set(&reachings)
 }
 
-func (self *reachingState) Advance() ([]EvmState, error) {
+func (self reachingState) Advance() ([]EvmState, error) {
+	log.Printf("Entering block at %d with stack height %v", self.nextBlock.Offset, self.stack.Height())
 	pc := self.nextBlock.Offset
-	stack := self.stack
+	st := self.stack
 	for i := range self.nextBlock.Instructions {
 		inst := &self.nextBlock.Instructions[i]
 		op := inst.Op
-		opFrames, newStack := stack.Popn(op.StackReads())
+		opFrames, newStack := stack.Popn(st, op.StackReads())
 		operands := make([]InstructionPointer, len(opFrames))
 		for i, frame := range opFrames {
-			operands[i] = *frame.Value.(*InstructionPointer)
+			operands[i] = frame.Value().(InstructionPointer)
 		}
 		updateReachings(inst, operands)
 
@@ -95,37 +98,41 @@ func (self *reachingState) Advance() ([]EvmState, error) {
 		case op == SELFDESTRUCT:
 			return nil, nil
 		case op.IsPush():
-			newStack = NewFrame(newStack, &InstructionPointer{self.nextBlock, i})
+			newStack = stack.NewFrame(newStack, InstructionPointer{self.nextBlock, i})
 		case op.IsDup():
 	        // Uses stack instead of newStack, because we don't actually want to pop all those elements
-			newStack = NewFrame(stack, stack.UpBy(op.StackReads() - 1).Value)
+			newStack = stack.NewFrame(st, stack.UpBy(st, op.StackReads() - 1).Value())
 		case op.IsSwap():
 	        // Uses stack instead of newStack, because we don't actually want to pop all those elements
-			newStack = stack.Swap(op.StackReads() - 1)
+			newStack = stack.Swap(st, op.StackReads() - 1)
 		case op == JUMP:
 			if !operands[0].Get().Op.IsPush() {
 				return nil, fmt.Errorf("%v: Could not determine jump location statically; source is %v", pc, operands[0].GetAddress())
 			}
-			return []EvmState{
-				&reachingState{
-					program: self.program,
-					nextBlock: self.program.JumpDestinations[int(operands[0].Get().Arg.Int64())],
-					stack: newStack,
-				},
-			}, nil
+			if dest, ok := self.program.JumpDestinations[int(operands[0].Get().Arg.Int64())]; ok {
+				return []EvmState{
+					reachingState{
+						program: self.program,
+						nextBlock: dest,
+						stack: newStack,
+					},
+				}, nil
+			}
+			return nil, nil
 		case op == JUMPI:
 			if !operands[0].Get().Op.IsPush() {
 				return nil, fmt.Errorf("%v: Could not determine jump location statically; source is %v", pc, operands[0].GetAddress())
 			}
-			ret := []EvmState{
-				&reachingState{
+			var ret []EvmState
+			if dest, ok := self.program.JumpDestinations[int(operands[0].Get().Arg.Int64())]; ok {
+				ret = append(ret, reachingState{
 					program: self.program,
-					nextBlock: self.program.JumpDestinations[int(operands[0].Get().Arg.Int64())],
+					nextBlock: dest,
 					stack: newStack,
-				},
+				})
 			}
 			if self.nextBlock.Next != nil {
-				ret = append(ret, &reachingState{
+				ret = append(ret, reachingState{
 					program: self.program,
 					nextBlock: self.nextBlock.Next,
 					stack: newStack,
@@ -134,22 +141,27 @@ func (self *reachingState) Advance() ([]EvmState, error) {
 			return ret, nil
 		default:
 			if op.StackWrites() == 1 {
-				newStack = NewFrame(newStack, &InstructionPointer{self.nextBlock, i})
+				newStack = stack.NewFrame(newStack, InstructionPointer{self.nextBlock, i})
 			} else if op.StackWrites() > 1 {
 				return nil, fmt.Errorf("Unexpected op %v makes %v writes to the stack", op, op.StackWrites())
 			}
 		}
 
+		// If the stack is too deep, abort
+		if st.Height() > 1024 {
+			return nil, nil
+		}
+
 		pc += op.OperandSize() + 1
-		stack = newStack
+		st = newStack
 	}
 
 	if self.nextBlock.Next != nil {
 		return []EvmState{
-			&reachingState{
+			reachingState{
 				program: self.program,
 				nextBlock: self.nextBlock.Next,
-				stack: stack,
+				stack: st,
 			},
 		}, nil
 	} else {
